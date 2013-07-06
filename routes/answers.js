0ca6dbecbc4ae5f36ccca6707dda66a249fab3fc
Sam <mirthquakes@gmail.com>
2013-07-06 17:44:10 -0500

    integrated sqlite into get all method

--- routes/answers.js
+++ routes/answers.js
@@ -1,10 +1,45 @@
 /*global exports*/
+/*global require*/
 
+var fs = require("fs");
+var sqlite = require("sqlite3");
 
+var dbPath = "./data/base.sqlite";
+
+var database = null;
+fs.exists(dbPath, function (exists) {
+	'use strict';
+	if (exists) {
+		database = new sqlite.Database(dbPath);
+	}
+});
 
 exports.findAll = function (req, res) {
 	'use strict';
-	res.send({test: "test"});
+	
+	if (database !== undefined && database !== null) {
+		
+		var query = 'SELECT * FROM ANSWERS';
+			
+		database.all(query, function (err, rows) {
+			if (err) {
+				throw err;
+			}
+			
+			var foundAnswers = [];
+			
+			rows.forEach(function (row) {
+				foundAnswers.push(row);
+			});
+			
+			res.send({ANSWERS: foundAnswers});
+			
+			
+		});
+		
+		
+	}
+	
 };
 
 exports.findByID = function (req, res) {
