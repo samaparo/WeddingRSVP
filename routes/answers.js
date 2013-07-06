1e0780fa52e1084ba6bdb982fd3aee295bf56a77
Sam <mirthquakes@gmail.com>
2013-07-06 16:01:46 -0500

    move route handlers to separate directory

---
+++ routes/answers.js
@@ -0,0 +1,10 @@
+/*global exports*/
+exports.findAll = function (req, res) {
+	'use strict';
+	res.send({test: "test"});
+};
+
+exports.findByID = function (req, res) {
+	'use strict';
+	res.send({test: "test"});
+};
